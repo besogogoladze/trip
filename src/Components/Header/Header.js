26c9f95d239f7c2be6b45c9ec325a6fb9275cdd3
besogogoladze <Gogoladzebeso8@gmail.com>
2024-03-05 20:03:23 +0100

    header change home link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,7 +8,7 @@ export class Header extends Component {
       <div className="header">
         <ul>
           <li>
-            <NavLink exact="true" to="/">
+            <NavLink exact="true" to="/trip">
               Home
             </NavLink>
           </li>

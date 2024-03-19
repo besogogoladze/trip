8ba9d47b0dbadce59f39610e5097b55e11badd8e
besogogoladze <Gogoladzebeso8@gmail.com>
2024-03-19 16:20:36 +0100

    changes of footer and about us

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import IPI from "../../Images/IPI.png";
+import { NavLink } from "react-router-dom";
 
 function Footer() {
   return (
@@ -9,12 +11,89 @@ function Footer() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        flexDirection: "column",
       }}
     >
-      <h3>Ecole IPI</h3>
-      <p>tstn1</p>
-      <p>Groupe IGS</p>
+      <div
+        style={{
+          width: "100%",
+          margin: "20px 0",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            margin: "100px 0",
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+          className="footer_up"
+        >
+          <div>
+            <h3>CONTACTS</h3>
+            <div>
+              <p>address</p>
+              <p>Email</p>
+              <p>tel</p>
+            </div>
+          </div>
+          <div>
+            <h3>IPI</h3>
+            <div>
+              <p>Ecole IPI</p>
+              <p>tstn1</p>
+              <p>Groupe IGS</p>
+            </div>
+          </div>
+          <div>
+            <h3>Menu</h3>
+            <div>
+              <h3>Ecole IPI</h3>
+              <p>tstn1</p>
+              <p>Groupe IGS</p>
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            width: "100%",
+            height: "1px",
+            backgroundColor: "#fff",
+            margin: "20px 0",
+          }}
+        ></div>
+        <div
+          style={{
+            width: "100%",
+            marginTop: "10px",
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+          className="footer_down"
+        >
+          <p style={{ color: "#645E58", fontWeight: "bold" }}>
+            Copyright © 2024,{" "}
+            <NavLink
+              style={{ fontSize: "20px", color: "#d33" }}
+              exact="true"
+              to="https://www.facebook.com/Bes096"
+            >
+              BG
+            </NavLink>{" "}
+            All rights reserved.
+          </p>
+          <img
+            style={{ width: "100px", backgroundColor: "#fff" }}
+            src={IPI}
+            alt="IPI"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Pages/About_Us/About_Us.js
+++ src/Pages/About_Us/About_Us.js
@@ -3,6 +3,13 @@ import { Container } from "@mui/system";
 import "./about_Us.css";
 import "../../index.css";
 import ImgBesiki from "../../Images/incognito.jpg";
+import Besiki from "../../Images/Besiki.JPEG";
+import Adja from "../../Images/Adja.jpg";
+import Ye from "../../Images/Ye.jpeg";
+import Georgia from "../../Images/georgia.png";
+import Burma from "../../Images/Burma.png";
+import Senegal from "../../Images/Senegal.png";
+
 function About_Us() {
   return (
     <Container
@@ -45,17 +52,20 @@ function About_Us() {
             }}
             className="groupeMembers_img_p"
           >
-            <img style={{ width: "250px" }} src={ImgBesiki} alt="img error" />
+            <img style={{ width: "200px" }} src={Besiki} alt={ImgBesiki} />
             <p className="kodeMonoFont">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-              eius earum ut molestias architecto voluptate aliquam nihil,
-              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-              tenetur error, harum nesciunt ipsum debitis quas aliquid.
-              Reprehenderit, quia.
+              Hello, my name is Besiki Gogoladze. I'm 27 years old. I'm from
+              Georgia{" "}
+              <img
+                src={Georgia}
+                style={{ width: "20px" }}
+                alt="Flag of georgia"
+              />
+              . I'm a student in IPI Computer Science. My goal is to become a
+              professional developer of web and web mobile. also, i am
+              interested in computers and Ai. I have been a rugby player for 12
+              years and i'm still a professional rugby player. My inspiration
+              words in life: "You will do everything if you really want it!".
             </p>
           </div>
         </li>
@@ -77,17 +87,16 @@ function About_Us() {
             }}
             className="groupeMembers_img_p"
           >
-            <img style={{ width: "250px" }} src={ImgBesiki} alt="img error" />
+            <img style={{ width: "200px" }} src={Adja} alt={ImgBesiki} />
             <p className="kodeMonoFont">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-              eius earum ut molestias architecto voluptate aliquam nihil,
-              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-              tenetur error, harum nesciunt ipsum debitis quas aliquid.
-              Reprehenderit, quia.
+              My name is Adja Ndiaye, I'm 19 years old, and I'm from Senegal{" "}
+              <img src={Senegal} style={{ width: "20px" }} alt={ImgBesiki} />.
+              Currently, I'm a student in computer science at IPI Toulouse,
+              which is part of the IGS groupe. I'm really into technology, and
+              I'm passionate about computers. I enjoy paying attention to
+              details and learning new things. I'm determined to succeed in my
+              studies and hope to make a meaningful impact in the field of
+              technology someday.
             </p>
           </div>
         </li>
@@ -109,17 +118,18 @@ function About_Us() {
             }}
             className="groupeMembers_img_p"
           >
-            <img style={{ width: "250px" }} src={ImgBesiki} alt="img error" />
+            <img style={{ width: "200px" }} src={Ye} alt={ImgBesiki} />
             <p className="kodeMonoFont">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-              eius earum ut molestias architecto voluptate aliquam nihil,
-              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-              tenetur error, harum nesciunt ipsum debitis quas aliquid.
-              Reprehenderit, quia.
+              Hi Everybody, My name is Ye Wint Hlaing. I am 22 years olds. I am
+              from Myanmar{" "}
+              <img src={Burma} style={{ width: "20px" }} alt={ImgBesiki} />
+              (Burma). Now, I am studying Bachelor of information Technology in
+              IPI Toulouse. Because I’m really interested in coding and
+              developing Website, Application. That’s why I choose this major in
+              IPI Toulouse. I play football and chess with my friend, in my free
+              time. I really like to read books about military, history and some
+              novels. I want to become software engineer In future, when I
+              finish my Bachelor at IPI Toulouse.
             </p>
           </div>
         </li>

30feb8335571054f419d5de55c61c41d83299abb
besogogoladze <Gogoladzebeso8@gmail.com>
2024-04-25 09:28:59 +0200

    Updates

---
+++ static/js/231.bfe025c5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblog_porto_by_insestos=self.webpackChunkblog_porto_by_insestos||[]).push([[231],{2386:(s,t,r)=>{r.r(t),r.d(t,{default:()=>o});r(5043);const e=r.p+"static/media/Error404.2f8c7cd5d940c8edf0bc.jpeg";var c=r(579);const o=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("img",{style:{width:"100%"},src:e,alt:"Error"})})}}}]);
+//# sourceMappingURL=231.bfe025c5.chunk.js.map

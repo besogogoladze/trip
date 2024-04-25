4e622207dd82f150a083f9ff2692828a0faea523
besogogoladze <Gogoladzebeso8@gmail.com>
2024-04-25 10:09:15 +0200

    change trip page

--- src/Pages/Trip/Trip.js
+++ src/Pages/Trip/Trip.js
@@ -1,11 +1,78 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Skeleton from "@mui/material/Skeleton";
 
-function Trip() {
+function Media(props) {
+  const { loading = false } = props;
+
   return (
-    <div>
-      Trip page
+    <div style={{ width: "300px" }}>
+      <Card sx={{ maxWidth: 345, width: "100%", }}>
+        {loading ? (
+          <Skeleton
+            sx={{ height: 190 }}
+            animation="wave"
+            variant="rectangular"
+          />
+        ) : (
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512"
+            alt="Nicola Sturgeon on a TED talk stage"
+          />
+        )}
+
+        <CardContent>
+          {loading ? (
+            <React.Fragment>
+              <Skeleton
+                animation="wave"
+                height={10}
+                style={{ marginBottom: 6 }}
+              />
+              <Skeleton animation="wave" height={10} width="80%" />
+            </React.Fragment>
+          ) : (
+            <Typography variant="body2" color="text.secondary" component="p">
+              {
+                "Why First Minister of Scotland Nicola Sturgeon thinks GDP is the wrong measure of a country's success:"
+              }
+            </Typography>
+          )}
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default Trip
+Media.propTypes = {
+  loading: PropTypes.bool,
+};
+
+export default function Trip() {
+  return (
+    <div
+      style={{
+        margin: "50px auto",
+        width: "100%",
+        maxWidth: "1400px",
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        flexWrap: "wrap",
+        gap:'20px'
+      }}
+    >
+      <Media loading />
+      <Media loading />
+      <Media loading />
+      <Media loading />
+      {/* <Media /> */}
+    </div>
+  );
+}

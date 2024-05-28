3606a20bd61dd0039187472760f7c8a359d1b796
besogogoladze <Gogoladzebeso8@gmail.com>
2024-05-29 01:05:17 +0200

    change trip zoom

--- src/Pages/Trip/Trip.js
+++ src/Pages/Trip/Trip.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Suspense } from "react";
 import PropTypes from "prop-types";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -6,7 +6,6 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Skeleton from "@mui/material/Skeleton";
 import { Zoom } from "react-awesome-reveal";
-import { useState } from "react";
 import flight_day from "../../Images/trip/voyage/image1.jpeg";
 import day1 from "../../Images/trip/Day1/image12.jpeg";
 import day2 from "../../Images/trip/church/image5.jpeg";
@@ -24,32 +23,36 @@ function Media({ ...props }) {
       <div style={{ width: "300px" }}>
         <Zoom triggerOnce={true}>
           <Card sx={{ maxWidth: 345, width: "100%" }}>
-            {props.loading ? (
-              <Skeleton
-                sx={{ height: 190 }}
-                animation="wave"
-                variant="rectangular"
-              />
-            ) : (
+            <Suspense
+              fallback={
+                <Skeleton
+                  sx={{ height: 190 }}
+                  animation="wave"
+                  variant="rectangular"
+                />
+              }
+            >
               <CardMedia
                 component="img"
                 height="300"
                 image={props.img}
                 alt="Error"
               />
-            )}
+            </Suspense>
 
             <CardContent>
-              {props.loading ? (
-                <React.Fragment>
-                  <Skeleton
-                    animation="wave"
-                    height={10}
-                    style={{ marginBottom: 6 }}
-                  />
-                  <Skeleton animation="wave" height={10} width="80%" />
-                </React.Fragment>
-              ) : (
+              <Suspense
+                fallback={
+                  <React.Fragment>
+                    <Skeleton
+                      animation="wave"
+                      height={10}
+                      style={{ marginBottom: 6 }}
+                    />
+                    <Skeleton animation="wave" height={10} width="80%" />
+                  </React.Fragment>
+                }
+              >
                 <Typography
                   variant="body2"
                   color="text.secondary"
@@ -57,7 +60,7 @@ function Media({ ...props }) {
                 >
                   {props.text}
                 </Typography>
-              )}
+              </Suspense>
             </CardContent>
           </Card>
         </Zoom>
@@ -71,13 +74,6 @@ Media.propTypes = {
 };
 
 export default function Trip() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  }, []);
   return (
     <div
       id="flex_container"
@@ -92,54 +88,18 @@ export default function Trip() {
         gap: "20px",
       }}
     >
+      <Media img={flight_day} text="Departure day" linkTo={"/departure_day"} />
+      <Media img={day1} text="Day 1 (Dom Luis I Bridge)" linkTo={"/day1"} />
       <Media
-        loading={loading}
-        img={flight_day}
-        text="Departure day"
-        linkTo={"/departure_day"}
-      />
-      <Media
-        loading={loading}
-        img={day1}
-        text="Day 1 (Dom Luis I Bridge)"
-        linkTo={"/day1"}
-      />
-      <Media
-        loading={loading}
         img={day2}
         text="Day 2 (Church Saint Ildefonso)"
         linkTo={"/day2"}
       />
-      <Media
-        loading={loading}
-        img={day3}
-        text="Day 3 (Panda Gathering Time)"
-        linkTo={"/day3"}
-      />
-      <Media
-        loading={loading}
-        img={day4}
-        text="Day 4 (Panda Plays Football)"
-        linkTo={"/day4"}
-      />
-      <Media
-        loading={loading}
-        img={day5}
-        text="Day 5 (What is Metyis)"
-        linkTo={"/day5"}
-      />
-      <Media
-        loading={loading}
-        img={day7}
-        text="Day 7 (Boat tour in Aveiro)"
-        linkTo={"/day7"}
-      />
-      <Media
-        loading={loading}
-        img={day8}
-        text="Day 8 (Panda and Dragon)"
-        linkTo={"/day8"}
-      />
+      <Media img={day3} text="Day 3 (Panda Gathering Time)" linkTo={"/day3"} />
+      <Media img={day4} text="Day 4 (Panda Plays Football)" linkTo={"/day4"} />
+      <Media img={day5} text="Day 5 (What is Metyis)" linkTo={"/day5"} />
+      <Media img={day7} text="Day 7 (Boat tour in Aveiro)" linkTo={"/day7"} />
+      <Media img={day8} text="Day 8 (Panda and Dragon)" linkTo={"/day8"} />
 
       {/* <Media /> */}
     </div>

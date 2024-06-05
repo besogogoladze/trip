97b2e7dfed092e58873c4fe67a7853ff5e7f54aa
besogogoladze <Gogoladzebeso8@gmail.com>
2024-06-05 13:38:08 +0200

    change text on home page

--- src/Pages/Home/SectionOne/SectionTwo.js
+++ src/Pages/Home/SectionOne/SectionTwo.js
@@ -27,10 +27,10 @@ function SectionTwo() {
         </div>
         <Fade direction="down" duration={2000}>
           <p style={{ margin: "50px 0" }} className="kodeMonoFont">
-            All the members in INSENTOS will take the
-            software and website development the next year ( at IPI ). Our group
-            is really fond of learning and creating websites and softwares.
-            That's why we chose this topic to present to you.
+            All INSENTOS members will choose software and website development
+            next year (at IPI). Our group is really fond of learning and
+            creating websites and apps. We want to give you some information
+            about development. That is why we have chosen this topic for you.
           </p>
         </Fade>
         <Fade direction="down" triggerOnce={true} duration={2000}>
